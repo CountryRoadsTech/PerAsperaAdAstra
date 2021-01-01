0e2fd412ff6b5b1840f22469bcd20a4443c1cb47
Addison Martin <addisonejmartin@gmail.com>
2021-01-01 08:53:24 -0500

    Removed Rails project to begin again, this time setting up all basic Rail's things BEFORE beginning installation of turbo rails and stimulus.

--- app/controllers/application_controller.rb
+++
@@ -1,7 +0,0 @@
-class ApplicationController < ActionController::Base
-  # Enables protection from cross-site request forgery (CSRF) attacks.
-  protect_from_forgery with: :exception
-
-  # Ensures the user is signed in before performing any action.
-  before_action :authenticate_user!
-end
