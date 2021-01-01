c985e05a87467ebaafaa5f55f8855473464e2ed3
Addison Martin <addisonejmartin@gmail.com>
2021-01-01 09:51:33 -0500

    Installed and setup Capybara for system/acceptance tests.

--- Gemfile
+++ Gemfile
@@ -31,5 +31,10 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
 
+group :test do
+  gem 'capybara', '~> 3.34'
+  gem 'selenium-webdriver', '~> 3.142'
+end
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,6 +11,10 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 end
 
+require 'capybara/rails'
+require 'capybara/rspec'
+Capybara.default_driver = :selenium
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
