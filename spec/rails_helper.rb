8f619c70ffd9a1f7576abe32af5c1527de063042
Addison Martin <addisonejmartin@gmail.com>
2021-01-01 10:40:37 -0500

    Configured Rspec to include user authentication methods in feature tests.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -15,6 +15,10 @@ require 'capybara/rails'
 require 'capybara/rspec'
 Capybara.default_driver = :selenium
 
+RSpec.configure do |config|
+  config.include Devise::Test::IntegrationHelpers, type: :feature
+end
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
