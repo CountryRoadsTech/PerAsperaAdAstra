c5997335c540fe0144f951590c09d10bf5cd8098
Addison Martin <addisonejmartin@gmail.com>
2021-01-03 17:25:14 -0500

    Finished rendering 3d scene and added animation.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -50,6 +50,25 @@ window.addEventListener('turbo:load', function () {
     document.body.appendChild( renderer.domElement );
 });
 
+// Add a cube.
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
 // Use 3D orbit controls to move the camera around.
 //import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 //const controls = new OrbitControls();
+
+function animate() {
+    requestAnimationFrame( animate );
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render( scene, camera );
+}
+
+animate();
